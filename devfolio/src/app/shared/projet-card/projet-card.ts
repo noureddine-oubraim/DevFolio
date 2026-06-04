a2275f7fb448c316980bde93597486a5e57f0e25
rafaabennour14@gmail.com <rafaabennour14@gmail.com>
2026-06-04 02:11:15 +0100

    reset

--- devfolio/src/app/shared/projet-card/projet-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-projet-card',
-  imports: [],
-  templateUrl: './projet-card.html',
-  styleUrls: ['./projet-card.css'],
-})
-export class ProjetCard {}
